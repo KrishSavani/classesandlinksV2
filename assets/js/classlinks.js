31f7019d89a0e6ce32ce901f188afbd83d196136
Krish Savani <74283604+KrishSavani@users.noreply.github.com>
2021-09-17 18:49:58 +0530

    Added classlink.js

---
+++ assets/js/classlinks.js
@@ -0,0 +1,85 @@
+// OG function
+// function name(){
+//     var x1_lms ="";
+//     var y1_ch="";
+//     var z2_m="";
+//         var x =window.open(y1_ch,'_blank');
+//         setTimeout(function() { x.close(); }, 1000);
+//         var y =window.open(z2_m,'_blank');
+//         setTimeout(function() { y.close(); }, 1000);
+//         var z =window.open(x1_lms,'_blank');
+//     };
+
+
+
+// test new krish
+
+function ds() {
+    var dslms = "http://lms.nuv.ac.in/mod/attendance/view.php?id=4540";
+    var dsm = "https://teams.microsoft.com/l/meetup-join/19:dtvb-JsD1Q70Gh7_WDs_B1bNaoT8cCoRaa99MBQktc81@thread.tacv2/1631689493432?context=%7B%22Tid%22:%22d1b99a31-59dc-4039-b9c4-f6166b6bf0a4%22,%22Oid%22:%22570183d8-e3e1-4d83-8e4a-79dd2974c5c8%22%7D";
+    var z = window.open(dslms, '_blank');
+    var x = window.open(dsm, '_blank');
+    setTimeout(function() { x.close(); }, 60000);
+};
+
+function m() {
+    var m = "https://teams.microsoft.com/l/channel/19%3Ad7de3a5addc941a3ad6042da8b392a74%40thread.tacv2/Class?groupId=f400bb3a-e915-48a8-ba55-08f79746f6a4&tenantId=d1b99a31-59dc-4039-b9c4-f6166b6bf0a4";
+    var x = window.open(m, '_blank');
+    setTimeout(function() { x.close(); }, 5000);
+
+};
+
+function evs() {
+    var evslms = "http://lms.nuv.ac.in/mod/forum/view.php?id=3571";
+    var z = window.open(evslms, '_blank');
+};
+
+function imp() {
+    var implms = "http://lms.nuv.ac.in/mod/attendance/view.php?id=6863";
+    var impm = "https://teams.microsoft.com/l/meetup-join/19%3afi8ZDytMwIH9KlB7t4TPOHH4NSd7W0OW5oVBM0LgYOI1%40thread.tacv2/1627444671553?context=%7b%22Tid%22%3a%22d1b99a31-59dc-4039-b9c4-f6166b6bf0a4%22%2c%22Oid%22%3a%220ffee5fa-5f48-4870-b69e-b9556aa305c3%22%7d";
+    var z = window.open(implms, '_blank');
+    var x = window.open(impm, '_blank');
+    setTimeout(function() { x.close(); }, 60000);
+};
+
+function oop() {
+    var oop = "https://teams.microsoft.com/l/channel/19%3AAR_O3WiZtPLaFDPwYOk1cChD84QttIKh0vB-p3szOcs1%40thread.tacv2/General?groupId=88d4dc00-4b30-42a8-a7dd-3668f7894f20&tenantId=d1b99a31-59dc-4039-b9c4-f6166b6bf0a4";
+    var x = window.open(oop, '_blank');
+    setTimeout(function() { x.close(); }, 5000);
+
+};
+
+function id1() {
+    var id1 = "https://teams.microsoft.com/l/channel/19%3AA7HCqlyBPDQAgOR94NVOF9hBpWi2YPubXY7pHXeCV0Q1%40thread.tacv2/General?groupId=29d70960-386d-40f3-bba9-6fd135b959fb&tenantId=d1b99a31-59dc-4039-b9c4-f6166b6bf0a4";
+    var x = window.open(id1, '_blank');
+    setTimeout(function() { x.close(); }, 5000);
+
+};
+
+function ooplab1() {
+    var ooplab1 = "https://teams.microsoft.com/l/channel/19%3AAR_O3WiZtPLaFDPwYOk1cChD84QttIKh0vB-p3szOcs1%40thread.tacv2/General?groupId=88d4dc00-4b30-42a8-a7dd-3668f7894f20&tenantId=d1b99a31-59dc-4039-b9c4-f6166b6bf0a4";
+    var x = window.open(ooplab1, '_blank');
+    setTimeout(function() { x.close(); }, 5000);
+
+};
+
+function ooplab2() {
+    var ooplab2 = "https://teams.microsoft.com/l/channel/19%3AAR_O3WiZtPLaFDPwYOk1cChD84QttIKh0vB-p3szOcs1%40thread.tacv2/General?groupId=88d4dc00-4b30-42a8-a7dd-3668f7894f20&tenantId=d1b99a31-59dc-4039-b9c4-f6166b6bf0a4";
+    var x = window.open(ooplab2, '_blank');
+    setTimeout(function() { x.close(); }, 5000);
+
+};
+
+function dslab1() {
+    var dslab1 = "";
+    var x = window.open(dslab1, '_blank');
+    setTimeout(function() { x.close(); }, 5000);
+
+};
+
+function dslab2() {
+    var dslab2 = "https://teams.microsoft.com/l/channel/19%3Adtvb-JsD1Q70Gh7_WDs_B1bNaoT8cCoRaa99MBQktc81%40thread.tacv2/General?groupId=d19f78c6-3c24-4948-a6ee-dcee2834f7c8&tenantId=d1b99a31-59dc-4039-b9c4-f6166b6bf0a4";
+    var x = window.open(dslab2, '_blank');
+    setTimeout(function() { x.close(); }, 5000);
+
+};
